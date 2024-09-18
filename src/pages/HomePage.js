f95ffefb378492bbf23b68f7b0b77cae67c4821e
Nico Elvino <nicoelvino@gmail.com>
2024-09-18 11:37:49 +1000

    initial setup for job seeker project, with pages components and initial GraphQL queries

---
+++ src/pages/HomePage.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+import JobList from '../components/JobList';
+import { getJobs } from '../lib/graphql/queries';
+
+getJobs().then((jobs) => console.log('jobs:', jobs));
+
+function HomePage() {
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    getJobs().then(setJobs);
+  }, []);
+
+  return (
+    <div>
+      <h1 className="title">
+        Job Board
+      </h1>
+      <JobList jobs={jobs} />
+    </div>
+  );
+}
+
+export default HomePage;
